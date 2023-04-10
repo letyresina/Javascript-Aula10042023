8a3f3f4b5160257f8de99de04601732d4740b500
letyresina <leticiaresina@gmail.com>
2023-04-10 11:06:36 -0300

    Treinando While e While-Do

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// While
+
+let cont = 0;
+while(cont <= 10){
+    document.write("O valor do While é: " + cont + "<br>");
+    cont++;
+}
+
+// Do-While
+
+let cont1 = 0;
+do{
+    document.write("O valor do While-Do é: " + cont1 + "<br>");
+    cont1++
+} while (cont1 == 0)
